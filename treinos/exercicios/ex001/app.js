279a1b9325ba8520916b0081872a3f7e9ad0a94f
Angelo Nascimento <angeloinacio321@gmail.com>
2025-09-02 19:29:03 -0300

    completando exe001

--- treinos/exercicios/ex001/app.js
+++ treinos/exercicios/ex001/app.js
@@ -4,3 +4,25 @@
 -EXIBIR A HORA EM TEMPO REAL DE ACORDO COM O SISTEMA
 -
 */
+let horaAtual = new Date();
+let hora = horaAtual.getHours();
+let imagem = document.getElementById('img');
+let mensagem = document.getElementById('msg');
+let fundo = document.getElementById('principal')
+
+
+if (hora < 12){
+    mensagem.innerText = `São extamente ${hora} horas`;
+    fundo.style.background = 'aqua';
+}else if(hora <= 18){
+    mensagem.innerText = `São extamente ${hora} horas`;
+    fundo.style.background = '#d84d78';
+    imagem.src = 'img/tarde.jpeg';
+    
+}else{
+    mensagem.innerText = `São extamente ${hora} horas`;
+    fundo.style.background = '#515151';
+    imagem.src = 'img/noite.jpeg';
+}
+
+
